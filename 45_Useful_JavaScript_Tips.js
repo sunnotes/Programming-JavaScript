0b3c97d0bfac29e9ab310e843e76ff49c49bbb32
eason <liliangjun88@gmail.com>
2014-01-10 12:50:09 +0800

    init

---
+++ 45_Useful_JavaScript_Tips.js
@@ -0,0 +1,44 @@
+//http://flippinawesome.org/2013/12/23/45-useful-javascript-tips-tricks-and-best-practices/
+
+console.log([ 10 ] === 10); // is false
+
+var flag;
+if ([ 10 ] === 10) {
+	flag = 'eq';
+} else {
+	flag = 'noteq';
+};
+
+
+
+
+
+
+function getCategory(age) {  
+    var category = "";  
+    switch (true) {  
+        case isNaN(age):  
+            category = "not an age";  
+            break;  
+        case (age >= 50):  
+            category = "Old";  
+            break;  
+        case (age <= 20):  
+            category = "Baby";  
+            break;  
+        default:  
+            category = "Young";  
+            break;  
+    };  
+    return category;  
+}  
+getCategory(5);  // will return "Baby"
+
+// is false
+// [10] == 10 // is true
+// '10' == 10 // is true
+// '10' === 10 // is false
+// [] == 0 // is true
+// [] === 0 // is false
+// '' == false // is true but true == "a" is false
+// '' === false // is false
